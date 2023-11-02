4bd75e8910ea9eca400f5e232b55362475422371
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-11-02 09:31:52 -0700

    betty fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,8 +30,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	int right = 0, left = 0, total = 0;
 
-	return ((int)(binary_tree_height(tree->left) - binary_tree_height(tree->right)));
+	if (tree)
+	{
+		left = ((int)binary_tree_height(tree->left));
+		right = ((int)binary_tree_height(tree->right));
+		total = left - right;
+	}
+	return (total);
 }
